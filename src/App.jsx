e591b7c6a395382e60d7b2692e09035863d9ff91
YuriAyres <yuriadp7@gmail.com>
2026-07-08 18:55:59 -0300

    Ajuste da estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentScreen, setCurrentScreen] = useState('intro');
 
   return (
-    <>
-    </>
-  )
+    <main>
+      {currentScreen === 'intro' && (
+        <div>
+          Intro Screen
+        </div>
+      )}
+
+      {currentScreen === 'loading' && (
+        <div>
+          Loading Animation
+        </div>
+      )}
+
+      {currentScreen === 'portfolio' && (
+        <div>
+          Portfolio
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
