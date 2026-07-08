064e4a4349e5ce5fbe15f77604a0634b0ddb42c8
YuriAyres <yuriadp7@gmail.com>
2026-07-08 18:41:43 -0300

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Projeto Exemplo",
+    description: "Descrição do projeto",
+    technologies: ["React", "Node.js"],
+    github: "",
+    demo: "",
+    image: ""
+  }
+];
